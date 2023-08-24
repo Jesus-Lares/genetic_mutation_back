92d9177c234900371c1e0451f8297357017e6a2e
Jesús Lares <a18310448@ceti.mx>
2023-08-24 08:32:06 +0000

    Build: frontend 24 8 23 #20

--- src/core/routes/index.ts
+++ src/core/routes/index.ts
@@ -1,20 +1,19 @@
 import { Router, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
+import path from "path";
 
 import env from "@config/env";
 import swaggerSetup from "@config/swagger";
 import cleanReqInputs from "@helpers/sanitizeRequestInput";
 import { ROUTE_DOCUMENTATION } from "@constants";
 
-import path from "path";
 import routesV1 from "./v1/routes";
 
 export default (router: Router): void => {
-  /* router.use(/^\/(?!api)., (_: Request, res: Response) => {
+  router.use(/^\/(?!api).*/, (_: Request, res: Response) => {
     const indexPath = path.join(process.cwd(), "public", "index.js");
     res.sendFile(indexPath);
   });
-  */
 
   router.use("/api/v1", cleanReqInputs, routesV1);
 
